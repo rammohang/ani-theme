1ca966d9269a82b01aae2f84425ac590e42af138
rammohang <rammohan.greddy@gmail.com>
2015-12-23 14:56:46 +0530

    save user credentials in root scope after successful authentication..

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,13 +7,20 @@
  * # MainCtrl
  * Controller of yapp
  */
-app.controller('LoginCtrl', function($scope, $location) {
+app.controller('LoginCtrl', function($scope, $location, $rootScope) {
 
     $scope.submit = function() {
-
-      $location.path('/dashboard');
-
-      return false;
+    	
+		$rootScope.userId = $scope.userId;
+		$rootScope.password = $scope.password;
+		
+		// write authentication api call here.
+		$userLoggedIn = true; // this should be set to true, if authentication is successful 
+		
+		// redirect to dashboard upon successful authentication.
+		$location.path('/dashboard');
+	
+		return false;
     }
 
   });
