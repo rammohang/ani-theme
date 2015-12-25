3338b68890aba7a2ed9f204ed74bbc9d275d777c
raviteja548 <raviteja.m548@gmail.com>
2015-12-25 22:01:28 +0530

    Added code to display status of every operation - BackUpOrg

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,11 @@
  */
 var app = angular.module('yapp', [ 'ngRoute', 'ngAnimate' ]);
 
+app.config(function($httpProvider) {
+	 $httpProvider.defaults.timeout = 50000;
+});
+
+
 app.config(function($routeProvider) {
 	$routeProvider.when('/login', {
 		templateUrl : 'views/login.html',
@@ -47,7 +52,7 @@ app.config(function($routeProvider) {
 
 // declare global constants here
 app.run(function($rootScope) {
-	$rootScope.baseUrl = "http://172.16.203.103:8080/apigee_rest/services/";
+	$rootScope.baseUrl = "http://localhost:8084/apigee_rest/services/";
 	$rootScope.userName = "mraviteja48@gmail.com";
 	$rootScope.password = "Ravi548$";
 });
@@ -186,6 +191,12 @@ app.controller('GetProxyCtrl', function($scope, $location, $rootScope, $http) {
 });
 
 app.controller('BackUpOrgCtrl', function($scope, $location, $rootScope, $http) {
+	$scope.backUpSuccess = "";
+	$scope.backUpApps = "";
+	$scope.backUpDev = "";
+	$scope.backUpProd = "";
+	$scope.backUpRes = "";
+	
 	$scope.proxyData = "";
 	$scope.backUpOrg = function() {
 		var commonConfiguration = {
@@ -196,12 +207,58 @@ app.controller('BackUpOrgCtrl', function($scope, $location, $rootScope, $http) {
 		};
 		console.log(commonConfiguration);
 		var responsePromise = $http.post($rootScope.baseUrl
-				+ "apigee/backuporg", commonConfiguration, {});
+				+ "apigee/backuporg?backup="+"apiProxy", commonConfiguration, {});
 		responsePromise.success(function(data, status, headers, config) {
-			$scope.organization = "";
-			$scope.apiProxyName = "";
-			$scope.proxyData = data;
-			console.log($scope.proxyData);
+			$scope.backUpSuccess = "Backup Of API Proxies has beeen done Successfully";
+			// call internally backup apps
+			var responsePromise = $http.post($rootScope.baseUrl
+					+ "apigee/backuporg?backup="+"apps", commonConfiguration, {});
+			responsePromise.success(function(data, status, headers, config) {
+				$scope.backUpApps+= "Backup Of APPS has beeen done Successfully\n";
+				//call internally backup dev
+				var responsePromise = $http.post($rootScope.baseUrl
+						+ "apigee/backuporg?backup="+"apiDevs", commonConfiguration, {});
+				responsePromise.success(function(data, status, headers, config) {
+					$scope.backUpDev+= "Backup Of Developers has beeen done Successfully\n";
+					// call of backup products
+					var responsePromise = $http.post($rootScope.baseUrl
+							+ "apigee/backuporg?backup="+"apiProds", commonConfiguration, {});
+					responsePromise.success(function(data, status, headers, config) {
+						$scope.backUpProd+= "Backup Of Products has beeen done Successfully\n";
+								// call of backup resources
+						var responsePromise = $http.post($rootScope.baseUrl
+								+ "apigee/backuporg?backup="+"apiRes", commonConfiguration, {});
+						responsePromise.success(function(data, status, headers, config) {
+							$scope.backUpRes+= "Backup Of Resources has beeen done Successfully\n";
+							$scope.organization = "";
+							$scope.backUpLocation = "";
+							$scope.proxyData = data;
+							console.log($scope.proxyData);
+						});
+						responsePromise.error(function(data, status, headers, config) {
+							alert("Submitting form failed!");
+						});
+								// resources call done
+					});
+					responsePromise.error(function(data, status, headers, config) {
+						alert("Submitting form failed!");
+					});
+							
+					// products backup done
+				});
+				responsePromise.error(function(data, status, headers, config) {
+					alert("Submitting form failed!");
+				});
+						
+				
+				
+				
+				//backup dev done
+			});
+			responsePromise.error(function(data, status, headers, config) {
+				alert("Submitting form failed!");
+			});
+			//internal call done
 		});
 		responsePromise.error(function(data, status, headers, config) {
 			alert("Submitting form failed!");
